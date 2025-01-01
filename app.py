0c90d6b687c35370fd77d881ab30ee65db0891e9
Dalton Ma <85321730+bohrium2b@users.noreply.github.com>
2025-01-01 20:15:25 +0000

    Small fixes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import requests
 from flask import Flask, request, jsonify, render_template, redirect
 import json
 from oauthlib.oauth2 import WebApplicationClient
 from argparse import ArgumentParser
+import os 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 arg_parser = ArgumentParser()
 arg_parser.add_argument('--port', type=int, default=5000)
@@ -27,13 +30,14 @@ def authenticate(provider):
     if provider_config is None:
         return jsonify({'error': 'Provider not found'}), 404
     client = WebApplicationClient(provider_config['clientId'])
-    authorization_url = client.prepare_authorization_request(
+    request_uri = client.prepare_request_uri(
         provider_config['endpoints']['authorize'],
         redirect_uri=f"{config['redirectUri']}/redirect/{provider}",
         scope='openid profile email'
     )
+    print(request_uri)
     # Redirect the user to the authorization URL
-    return redirect(authorization_url)
+    return redirect(request_uri)
 
 
 @app.route('/redirect/<provider>')
@@ -48,11 +52,13 @@ def redirect_provider(provider):
         client_id=provider_config['clientId'],
         client_secret=provider_config['clientSecret']
     )
-    accesstoken = requests.get(provider_config['access_token'], data=data)
+    accesstoken = requests.get(provider_config['endpoints']['accessToken'], data=data)
     client.parse_request_body_response(accesstoken.text)
     header = {'Authorization': f"Bearer {client.token['access_token']}"}
     userinfo = requests.get(provider_config['userinfo'], headers=header)
     print(userinfo.json())
+    # Log the user in and redirect to the welcome page
+    request.session['username'] = userinfo.json()['name']
     return render_template('welcome.html', userinfo=userinfo.json())
 
 
